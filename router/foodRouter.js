6c98bf64cf226372e12d512aba309d4399bab606
Brandon_X <1499672553@qq.com>
2019-07-22 17:28:26 +0800

    关键字查询模块

--- router/foodRouter.js
+++ router/foodRouter.js
@@ -41,7 +41,7 @@ router.post('/add',(req,res)=>{
 
 /**
  * @api {post} /food/getInfoByType  食品分类查询模块
- * @apiName 食品查询模块
+ * @apiName getInfoByType
  * @apiGroup Food
  *
  * @apiParam {Spring} typeid 类名id
@@ -55,19 +55,33 @@ router.post('/getInfoByType',(req,res)=>{
     .then((data)=>{   //find 的返回值是个数组，每个元素是个数据对象
         res.send({code:200,msg:'查询成功',list:data});
     })
+    .catch(()=>{
+        res.send({code:199,msg:'查询失败'})
+    })
 })
 
 /**
- * @api {post} /food/getInfoByType  食品分类查询模块
- * @apiName 食品查询模块
+ * @api {post} /food/getInfoBykw  关键字查询模块
+ * @apiName getInfoBykw
  * @apiGroup Food
  *
- * @apiParam {Spring} typeid 类名id
+ * @apiParam {Spring} kw 关键字
  *
  * @apiSuccess {json} firstname {code:200,msg:'查询成功'}
  * @apiSuccess {String} lastname  Lastname of the User.
  */
-
+router.post('/getInfoBykw',(req,res)=>{
+    let {kw}=req.body;
+    let reg = new RegExp(kw);   //创建一个正则表达式   用于匹配关键字
+    // $set $gte $lte $or $and $regex   是MongoDB的查询语法
+    Food.find({$or:[{name:{$regex:reg}},{desc:{$regex:reg}}]}) //$or 能同时查询多个键
+    .then((data)=>{
+        res.send({code:200,msg:'查询成功',list:data});
+    })
+    .catch(()=>{
+        res.send({code:199,msg:'查询失败'});
+    })
+})
 
 
 
