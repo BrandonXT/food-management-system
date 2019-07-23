bc0d35c29d7f4369334f4d77b1b5382c681c2370
Brandon_X <1499672553@qq.com>
2019-07-23 08:58:40 +0800

    完善上传文件功能

--- router/fileRouter.js
+++ router/fileRouter.js
@@ -5,11 +5,13 @@ const multer = require('multer');
 var storage = multer.diskStorage({  //配置信息
     destination:function(req,file,cb){
         //指定保存文件夹路径
-        cb(null,'./uploads');
+        cb(null,'./mystatic/img');
     },
     filename:function(req,file,cb){
         //指定文件的名字
-        cb(null,'aaa.jpg');     //若不设置就不会加文件后缀格式
+        let ext = file.originalname.split('.')[1]; //取文件后缀
+        let tmpname=(new Date()).getTime()+(parseInt(Math.random()*9999));  //文件名防止重复
+        cb(null,`${tmpname}.${ext}`);     //若不设置就不会加文件后缀格式
     }
 });
 
@@ -24,12 +26,14 @@ router.post('/upload',upload.single('hehe'),(req,res)=>{
     let {mimetype,size,path}=req.file;   //获取上传的文件的信息
     let types = ['jpg','jpeg','png','gif'];   //允许上传的数据类型
     let tmpType = mimetype.split('/')[1];  //获取上传文件的类型
+    
     if(size>50000){
         return res.send({code:199,msg:'文件尺寸过大'});
     }else if(types.indexOf(tmpType)==-1){
         return res.send({code:198,msg:'文件类型错误'});
     }else{
-        res.send({code:200,msg:'上传成功'});
+        let url = `public/img/${req.file.filename}`; //拼接图片地址
+        res.send({code:200,msg:'上传成功',img:url});
     }
 });
 
